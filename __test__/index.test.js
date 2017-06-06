e240c35204bb7b87ee2ff8ca05122f5bf95c23a0
PsixokoT <psixo_kot@mail.ru>
2017-06-07 00:28:44 +0300

    add features tests

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -8,19 +8,19 @@ describe('translater Russian', () => {
   }
 
   it('should translate simple form', () => {
-      expect( translate(dictionaryRU, 'ru', 'key_1')).toEqual( "Первый текст" )
+    expect( translate(dictionaryRU, 'ru', 'key_1')).toEqual( "Первый текст" )
   })
 
   it('should translate plural form with count 1', () => {
-      expect( translate(dictionaryRU, 'ru', 'key_2', 1)).toEqual( "1 кошка" )
+    expect( translate(dictionaryRU, 'ru', 'key_2', 1)).toEqual( "1 кошка" )
   })
 
   it('should translate plural form with count 3', () => {
-      expect( translate(dictionaryRU, 'ru', 'key_2', 3)).toEqual( "3 кошки" )
+    expect( translate(dictionaryRU, 'ru', 'key_2', 3)).toEqual( "3 кошки" )
   })
 
   it('should translate plural form with count 7', () => {
-      expect( translate(dictionaryRU, 'ru', 'key_2', 7)).toEqual( "7 кошек" )
+    expect( translate(dictionaryRU, 'ru', 'key_2', 7)).toEqual( "7 кошек" )
   })
 })
 
@@ -32,19 +32,19 @@ describe('translater Polish', () => {
   }
 
   it('should translate simple form', () => {
-      expect( translate(dictionaryPL, 'pl', 'key_1')).toEqual( "Prosze, dwa bilety drugiej klasy do Warszawy." )
+    expect( translate(dictionaryPL, 'pl', 'key_1')).toEqual( "Prosze, dwa bilety drugiej klasy do Warszawy." )
   })
 
   it('should translate plural form with count 1', () => {
-      expect( translate(dictionaryPL, 'pl', 'key_2', 1)).toEqual( "1 godzina" )
+    expect( translate(dictionaryPL, 'pl', 'key_2', 1)).toEqual( "1 godzina" )
   })
 
   it('should translate plural form with count 3', () => {
-      expect( translate(dictionaryPL, 'pl', 'key_2', 3)).toEqual( "3 godziny" )
+    expect( translate(dictionaryPL, 'pl', 'key_2', 3)).toEqual( "3 godziny" )
   })
 
   it('should translate plural form with count 31', () => {
-      expect( translate(dictionaryPL, 'pl', 'key_2', 31)).toEqual( "31 godzin" )
+    expect( translate(dictionaryPL, 'pl', 'key_2', 31)).toEqual( "31 godzin" )
   })
 })
 
@@ -56,19 +56,19 @@ describe('translater English', () => {
   }
 
   it('should translate simple form', () => {
-      expect( translate(dictionaryEN, 'en', 'key_1')).toEqual( "First text" )
+    expect( translate(dictionaryEN, 'en', 'key_1')).toEqual( "First text" )
   })
 
   it('should translate plural form with count 0', () => {
-      expect( translate(dictionaryEN, 'en', 'key_2', 0)).toEqual( "0 hours" )
+    expect( translate(dictionaryEN, 'en', 'key_2', 0)).toEqual( "0 hours" )
   })
 
   it('should translate plural form with count 1', () => {
-      expect( translate(dictionaryEN, 'en', 'key_2', 1)).toEqual( "1 hour" )
+    expect( translate(dictionaryEN, 'en', 'key_2', 1)).toEqual( "1 hour" )
   })
 
   it('should translate plural form with count 7', () => {
-      expect( translate(dictionaryEN, 'en', 'key_2', 7)).toEqual( "7 hours" )
+    expect( translate(dictionaryEN, 'en', 'key_2', 7)).toEqual( "7 hours" )
   })
 })
 
@@ -80,18 +80,18 @@ describe('translater French', () => {
   }
 
   it('should translate simple form', () => {
-      expect( translate(dictionaryFR, 'fr', 'key_1')).toEqual( "Ayant risqué une fois-on peut rester heureux toute la vie" )
+    expect( translate(dictionaryFR, 'fr', 'key_1')).toEqual( "Ayant risqué une fois-on peut rester heureux toute la vie" )
   })
 
   it('should translate plural form with count 0', () => {
-      expect( translate(dictionaryFR, 'fr', 'key_2', 0)).toEqual( "0 heure" )
+    expect( translate(dictionaryFR, 'fr', 'key_2', 0)).toEqual( "0 heure" )
   })
 
   it('should translate plural form with count 1', () => {
-      expect( translate(dictionaryFR, 'fr', 'key_2', 1)).toEqual( "1 heure" )
+    expect( translate(dictionaryFR, 'fr', 'key_2', 1)).toEqual( "1 heure" )
   })
 
   it('should translate plural form with count 7', () => {
-      expect( translate(dictionaryFR, 'fr', 'key_2', 7)).toEqual( "7 heures" )
+    expect( translate(dictionaryFR, 'fr', 'key_2', 7)).toEqual( "7 heures" )
   })
 })
